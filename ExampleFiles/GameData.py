d6afdbd1d4f9779a2796a2050181f5609b10cb7b
Phillip <35803538+TimeTravelPenguin@users.noreply.github.com>
2018-01-26 15:15:08 +1100

    Add files via upload

---
+++ ExampleFiles/GameData.py
@@ -0,0 +1,10 @@
+# Set game information
+game = 'smo'
+category = 'Darker_Side'
+
+# Query should be '?conditionA=1&condintionB=2&conditionC=3'
+# For more query filter options, visit:
+# https://github.com/speedruncomorg/api/blob/master/version1/runs.md#get-runs
+
+# For no filter, query = ''
+query = '?emulators=false'
