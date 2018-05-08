dbddf0c7ac20c4a7b6470884d13b3e651db922e1
Phillip <35803538+TimeTravelPenguin@users.noreply.github.com>
2018-05-08 17:54:05 +1000

    Delete PlayerMetadata.cs

--- SLE C#/Structs/PlayerMetadata.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SLEAPI.Internal
-{
-  internal sealed class PlayerMetadata
-  {
-    public string id { get; set; }
-    public string uri { get; set; }
-    public string name { get; set; }
-  }
-}
